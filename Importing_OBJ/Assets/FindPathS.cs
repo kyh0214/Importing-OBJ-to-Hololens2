9a23b2d39971f698c6516a80a782c718ff3c4851
kyh0214 <balentino123@gmail.com>
2022-04-18 16:24:16 +0900

    edit scripts

---
+++ Importing_OBJ/Assets/FindPathS.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Dummiesman;
+using System.IO;
+using System;
+
+
+public class FindPathS : MonoBehaviour
+{
+    string f_path;
+
+    public GameObject p;
+    //public GameObject p, s, d, e;
+    public GameObject c;
+    GameObject obj;
+
+    Transform posi;
+
+
+    void loading(string path)
+    {   //Application.streamingAssetsPath; 를 사용 따로 StreamingAssets폴더를 만들어줘야함
+        //Import OBJ via Application.streamingAssetsPath
+        var lodedobj = new OBJLoader().Load(path);          //경로에서 파일 로드
+        obj = GameObject.Find("Object3");
+        posi = obj.GetComponent<Transform>();
+        posi.position = new Vector3(-0.2f, 0.1f, 0.5f);  //눈앞에 보이게 transform 조정
+        posi.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+    }
+
+
+    void Start()
+    {
+        Instantiate(c, new Vector3(0f, 0.25f, 1f), Quaternion.identity);
+
+        f_path = Application.streamingAssetsPath;
+
+        string file_p;
+        string fp;
+        file_p = Path.Combine(f_path, "Object3.obj");
+        fp = file_p.Replace("\\", "/");
+        Debug.Log(fp);
+
+        if (File.Exists(fp))
+        {
+            loading(fp);
+        }
+        else if (!File.Exists(fp))
+        {
+            Instantiate(p, new Vector3(-0.1f, 0f, 0.5f), Quaternion.identity);//홀로렌즈에서 파일을 찾지 못하면 큐브를 생성하도록 트릭 설치
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Importing_OBJ/Assets/SeeDebug.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeeDebug : MonoBehaviour
+{
+    TextMesh textMesh;
+
+    
+    void Start()
+    {
+        textMesh = gameObject.GetComponent<TextMesh>();
+    }
+    private void OnEnable() //개체가 사용되도록 설정되고 활성상태가 되면 호출
+    {
+        Application.logMessageReceivedThreaded += LogMessage;
+    }
+    void OnDisable()         //동작을 사용할 수 없거나 비활성상태가 되면 호출
+    {
+        Application.logMessageReceivedThreaded -= LogMessage;
+    }
+
+    public void LogMessage(string message, string stackTrace, LogType type)
+    {
+        if (textMesh.text.Length > 300)
+        {
+            textMesh.text += message + "\n";//stackTrace는 근원지가 어떤 함수인지 알려준다
+        }
+        else
+        {
+            textMesh.text += message + "\n";
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+}
